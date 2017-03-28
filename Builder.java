68ada71a75221b81e948b04a20d43ca23de08480
fubai <54958512@qq.com>
2017-03-28 20:42:37 +0800

    建造者模式
    建造者模式的自我理解和实现

---
+++ Builder.java
@@ -0,0 +1,32 @@
+/**
+ * Created by zhang on 2017/3/28.
+ */
+public class Builder {
+
+    private Business business = new Business();
+
+    public Builder field1(String field1){
+        this.business.setField1(field1);
+        return this;
+    }
+    public Builder field2(String field2){
+        this.business.setField2(field2);
+        return this;
+    }
+    public Builder field3(String field3){
+        this.business.setField3(field3);
+        return this;
+    }
+    public Builder field4(String field4){
+        this.business.setField4(field4);
+        return this;
+    }
+    public Builder field5(String field5){
+        this.business.setField5(field5);
+        return this;
+    }
+
+    public Business buid(){
+        return business;
+    }
+}
---
+++ SMSBusiness.java
@@ -0,0 +1,11 @@
+/**
+ * Created by zhang on 2017/3/28.
+ */
+public class SMSBusiness extends Business {
+
+    private Builder builder = new Builder();
+
+    public Business createBusiness(){
+        return builder.field1("11").field2("12").field3("13").field4("14").field5("15").buid();
+    }
+}
