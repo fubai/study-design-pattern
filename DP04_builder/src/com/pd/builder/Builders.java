e55cca82489591b7c3cf0672e22fb994fe2807aa
zhang <54958512@qq.com>
2017-03-28 20:58:08 +0800

    建造者模式

---
+++ DP04_builder/src/com/pd/builder/Builders.java
@@ -0,0 +1,33 @@
+package com.pd.builder;
+/**
+ * Created by zhang on 2017/3/28.
+ */
+public class Builders {
+
+    private Business business = new Business();
+
+    public Builders field1(String field1){
+        this.business.setField1(field1);
+        return this;
+    }
+    public Builders field2(String field2){
+        this.business.setField2(field2);
+        return this;
+    }
+    public Builders field3(String field3){
+        this.business.setField3(field3);
+        return this;
+    }
+    public Builders field4(String field4){
+        this.business.setField4(field4);
+        return this;
+    }
+    public Builders field5(String field5){
+        this.business.setField5(field5);
+        return this;
+    }
+
+    public Business buid(){
+        return business;
+    }
+}
